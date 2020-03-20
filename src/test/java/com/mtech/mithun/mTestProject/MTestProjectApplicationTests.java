9f100162006c840ba497964af929aee1bd418f0d
mithunpm <mithunpm2588@gmail.com>
2020-03-20 13:16:14 +0400

    first commit

---
+++ src/test/java/com/mtech/mithun/mTestProject/MTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtech.mithun.mTestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
