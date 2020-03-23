7de72dd0d030cb0c1b1d386d3e9d55894a13f12d
mithunpm <mithunpm2588@gmail.com>
2020-03-23 17:47:47 +0400

    mutable

---
+++ src/main/java/com/mtech/mithun/mTestProject/mutable/MMutable.java
@@ -0,0 +1,16 @@
+package com.mtech.mithun.mTestProject.mutable;
+
+public final class MMutable {
+	
+	private final int id ;
+
+	public int getId() {
+		return id;
+	}
+	
+	public MMutable(int id)
+	{
+		this.id=id;
+	}
+	
+}
